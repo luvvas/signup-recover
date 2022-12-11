991ec8972f458efca80f33d62ef8c055b884b2be
luvvas <lucasmachado1@ufpr.br>
2022-12-11 10:33:13 -0300

    feat: add ResetPasswordRequest model

---
+++ Models/ResetPasswordRequest.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace signup_recover.Models
+{
+  public class ResetPasswordRequest
+  {
+    [Required]
+    public string Token { get; set; } = string.Empty;
+    [Required, MinLength(6, ErrorMessage = "Please enter at least 6 characters")]
+    public string Password { get; set; } = string.Empty;
+    [Required, Compare("Password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+  }
+}
